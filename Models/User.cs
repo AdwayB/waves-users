a910a53e70bcc88cb744b4b5a398574e8bca5233
AdwayB <adwaybyju@gmail.com>
2024-03-18 23:03:20 +0530

    verified ports and added user model

---
+++ Models/User.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace waves_users.Models;
+
+public enum UserType {
+    Admin,
+    User
+};
+
+public class User {
+  public Guid UserId { get; set; }
+
+  [Required]
+  [MinLength(3)]
+  [MaxLength(20)]
+  public string Username { get; set; } = string.Empty;
+  
+  // [JsonIgnore]
+  [Required]
+  [MinLength(8)]
+  [MaxLength(120)]
+  public string Password { get; set; } = string.Empty;
+  
+  [Required]
+  [MinLength(4)]
+  [MaxLength(20)]
+  public string LegalName { get; set; } = string.Empty;
+  
+  [Required]
+  [MinLength(4)]
+  [MaxLength(30)]
+  [EmailAddress]
+  [RegularExpression(@"^[a-zA-Z0-9.+_%$#&-]+@gmail\.com$", ErrorMessage = "Email address must be a valid Gmail address with allowed symbols.")]
+  public string Email { get; set; } = string.Empty;
+  
+  [Required]
+  [MinLength(7)]
+  [MaxLength(15)]
+  public string MobileNumber { get; set; } = string.Empty;
+  
+  [Required]
+  [MinLength(3)]
+  [MaxLength(3)]
+  public string Country { get; set; } = string.Empty;
+
+  [Required]
+  [MinLength(4)]
+  [MaxLength(5)]
+  public string Type { get; set; } = UserType.User.ToString();
+}
